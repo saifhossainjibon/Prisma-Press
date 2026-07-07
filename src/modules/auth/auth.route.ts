15231c8d59dfe9975b32bde3c5b70d49fc699e2c
saifhossainjibon <saifhossainjibon@gmail.com>
2026-07-07 23:57:11 +0600

    make file and structure of post and comment

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -3,5 +3,6 @@ import { authController } from "./auth.controller";
 
 const router = Router();
 router.post("/login", authController.loginUser)
+router.post("/refresh-token", authController.refreshToken)
 
 export const authRoutes = router;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { jwtUtils } from "../../utils/jwt";
 
@@ -43,7 +43,31 @@ const loginUser = async (payload: ILoginUser) => {
   );
   return { accessToken, refreshToken };
 };
+const refreshToken =async (refreshToken: string)=>{
+  const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.jwt_refresh_secret)
+  if(!verifiedRefreshToken.success){
+    throw new Error(verifiedRefreshToken.error)
+  }
+  const {id}= verifiedRefreshToken.data as JwtPayload;
+  const user =await prisma.user.findUniqueOrThrow({
+    where:{
+      id
+    }
+  })
+  if(user.activeStatus==="BLOCKED"){
+    throw new Error("User is blocked")
+  }
+  const jwtPayload={
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  }
+  const accessToken = jwtUtils.createToken(jwtPayload, config.jwt_access_secret, config.jwt_access_expires_in as SignOptions)
 
+  return {accessToken}
+
+}
 export const authService = {
-  loginUser,
+  loginUser, refreshToken
 };
